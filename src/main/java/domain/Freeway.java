63f6c1798593b0834f7ef650dcdb2adcbb44f42c
MeltingGlaciers <kirillganigin@mail.ru>
2021-04-02 22:40:46 +0300

    Delete merge FreewayProcess class into Freeway.

--- src/main/java/domain/Freeway.java
+++ src/main/java/domain/Freeway.java
@@ -4,10 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class Freeway {
 
     private Integer length;
+
+    List<Car> cars;
+    List<CrushRegistrar> crushRegistrars;
+
+    public Freeway(int length){
+
+        this.length = length;
+        cars = new LinkedList<>();
+        crushRegistrars = new ArrayList<>();
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
 }
---
+++ src/test/java/domain/FreewayTest.java
@@ -0,0 +1,24 @@
+package domain;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FreewayTest {
+
+    static Freeway f;
+
+    @BeforeClass
+    public static void setUp() {
+
+        f = new Freeway(10);
+
+    }
+
+    @Test
+    public void test(){
+
+        f.addCar(new Car(10));
+        assert f.getCars().size()==1;
+
+    }
+}

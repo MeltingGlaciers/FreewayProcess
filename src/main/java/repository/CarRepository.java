f1eaff78cc32075ac9a953b14f345c361fdf3595
MeltingGlaciers <kirillganigin@mail.ru>
2021-04-03 08:17:33 +0300

    Edit CarRepository and CarRepositoryImpl

--- src/main/java/repository/CarRepository.java
+++ src/main/java/repository/CarRepository.java
@@ -1,4 +1,15 @@
 package repository;
 
+import domain.Car;
+
+import java.util.List;
+
 public interface CarRepository {
+    List<Car> cars();
+
+    Car getById(Integer id);
+
+    void add(Car car);
+
+
 }
--- src/main/java/repository/CarRepositoryImpl.java
+++ src/main/java/repository/CarRepositoryImpl.java
@@ -1,7 +1,32 @@
 package repository;
 
+import domain.Car;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class CarRepositoryImpl implements CarRepository {
+    public final List<Car> cars = new ArrayList<>();
+
+    @Override
+    public List<Car> cars() {
+        return new ArrayList<>(cars);
+    }
+
+    @Override
+    public Car getById(Integer id) {
+        return cars.stream()
+                .filter(r -> r.getId().equals(id))
+                .findAny()
+                .orElse(null);
+    }
+
+    @Override
+    public void add(Car car){
+
+    }
+
+
 }
